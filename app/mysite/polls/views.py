f5512882714f18c3c331d51990cb36fede576aa7
tohidur <tohidur@urbanpiper.com>
2021-11-20 10:57:54 +0530

    write polls app - part 5 - more testing

--- app/mysite/polls/views.py
+++ app/mysite/polls/views.py
@@ -42,6 +42,13 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Exclude any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
--- base-dj-3.2/01-getting-started/02-writing-first-app/05-testing/README.md
+++ base-dj-3.2/01-getting-started/02-writing-first-app/05-testing/README.md
@@ -1,2 +1,8 @@
 - Add unit tests and flow tests as well
 - Better if you do test driven development
+
+- **To-do**  
+  - Intergrations test
+    - With selenium
+    - With Django LiveServerTestCase
+
